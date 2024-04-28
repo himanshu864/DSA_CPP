123fea38649eea6b421d9f96c7791ba5121edc17
himanshu864 <himanshu864000@gmail.com>
2024-04-28 10:14:37 +0530

    contest 395, daily guilt, bst lca

---
+++ BST/6.LCABST.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// if a node has one on left, and one on right, that is clearly the Lowest Common Ancestor
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        int a = p->val, b = q->val;
+        if (a > b)
+            swap(a, b);
+        while (root)
+        {
+            int x = root->val;
+            if (x == a || x == b || (x > a && x < b))
+                return root;
+            else if (x > a && x > b)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return NULL;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    TreeNode *root = new TreeNode(6);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(0);
+    root->left->right = new TreeNode(4);
+    root->left->right->left = new TreeNode(3);
+    root->right->left = new TreeNode(7);
+
+    TreeNode *p = root->left;
+    TreeNode *q = root->left->right->left;
+    cout << sol.lowestCommonAncestor(root, p, q)->val << endl;
+    return 0;
+}
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
