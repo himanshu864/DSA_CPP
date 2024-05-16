92107daf1012aa264200d6784ab5b65a9f1c777c
himanshu864 <himanshu864000@gmail.com>
2024-05-16 11:36:24 +0530

    Rush AVL idgaf

--- BST/AVL/CompleteAVL.cpp
+++ BST/AVL/CompleteAVL.cpp
@@ -18,13 +18,15 @@ public:
 
     AVL() { root = nullptr; }
 
-    // Helper methods for inserting
+    // Helper methods for inserting/deleting
     int NodeHeight(Node *p);
     int BalanceFactor(Node *p);
     Node *LLRotation(Node *p);
     Node *RRRotation(Node *p);
     Node *LRRotation(Node *p);
     Node *RLRotation(Node *p);
+    Node *InPre(Node *p);
+    Node *InSucc(Node *p);
 
     // Insert
     Node *rInsert(Node *p, int key);
@@ -33,6 +35,9 @@ public:
     void Inorder(Node *p);
     void Inorder() { Inorder(root); }
     Node *getRoot() { return root; }
+
+    // Delete
+    Node *Delete(Node *p, int key);
 };
 
 int AVL::NodeHeight(Node *p)
@@ -145,6 +150,24 @@ Node *AVL::RLRotation(Node *p)
     return prl;
 }
 
+Node *AVL::InPre(Node *p)
+{
+    while (p && p->rchild != nullptr)
+    {
+        p = p->rchild;
+    }
+    return p;
+}
+
+Node *AVL::InSucc(Node *p)
+{
+    while (p && p->lchild != nullptr)
+    {
+        p = p->lchild;
+    }
+    return p;
+}
+
 Node *AVL::rInsert(Node *p, int key)
 {
     Node *t;
@@ -196,48 +219,102 @@ void AVL::Inorder(Node *p)
     if (p)
     {
         Inorder(p->lchild);
-        cout << p->data << " ";
+        cout << p->data << ", " << flush;
         Inorder(p->rchild);
     }
+}
+
+Node *AVL::Delete(Node *p, int key)
+{
+    if (p == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (p->lchild == nullptr && p->rchild == nullptr)
+    {
+        if (p == root)
+        {
+            root = nullptr;
+        }
+        delete p;
+        return nullptr;
+    }
+
+    if (key < p->data)
+    {
+        p->lchild = Delete(p->lchild, key);
+    }
+    else if (key > p->data)
+    {
+        p->rchild = Delete(p->rchild, key);
+    }
+    else
+    {
+        Node *q;
+        if (NodeHeight(p->lchild) > NodeHeight(p->rchild))
+        {
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+
+    // Update height
+    p->height = NodeHeight(p);
+
+    // Balance Factor and Rotation
+    if (BalanceFactor(p) == 2 && BalanceFactor(p->lchild) == 1)
+    { // L1 Rotation
+        return LLRotation(p);
+    }
+    else if (BalanceFactor(p) == 2 && BalanceFactor(p->lchild) == -1)
+    { // L-1 Rotation
+        return LRRotation(p);
+    }
+    else if (BalanceFactor(p) == -2 && BalanceFactor(p->rchild) == -1)
+    { // R-1 Rotation
+        return RRRotation(p);
+    }
+    else if (BalanceFactor(p) == -2 && BalanceFactor(p->rchild) == 1)
+    { // R1 Rotation
+        return RLRotation(p);
+    }
+    else if (BalanceFactor(p) == 2 && BalanceFactor(p->lchild) == 0)
+    { // L0 Rotation
+        return LLRotation(p);
+    }
+    else if (BalanceFactor(p) == -2 && BalanceFactor(p->rchild) == 0)
+    { // R0 Rotation
+        return RRRotation(p);
+    }
+
+    return p;
 }
 
 int main()
 {
 
-    // LL Rotation
-    AVL tll;
-    tll.root = tll.rInsert(tll.root, 30);
-    tll.root = tll.rInsert(tll.root, 20);
-    tll.root = tll.rInsert(tll.root, 10);
+    AVL tree;
 
-    tll.Inorder();
+    int A[] = {10, 20, 30, 25, 28, 27, 5};
+    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++)
+    {
+        tree.root = tree.rInsert(tree.root, A[i]);
+    }
+
+    tree.Inorder();
     cout << endl;
 
-    // RR Rotation
-    AVL trr;
-    trr.root = trr.rInsert(trr.root, 10);
-    trr.root = trr.rInsert(trr.root, 20);
-    trr.root = trr.rInsert(trr.root, 30);
+    tree.Delete(tree.root, 28);
 
-    trr.Inorder();
-    cout << endl;
-
-    // LR Rotation
-    AVL tlr;
-    tlr.root = tlr.rInsert(tlr.root, 30);
-    tlr.root = tlr.rInsert(tlr.root, 10);
-    tlr.root = tlr.rInsert(tlr.root, 20);
-
-    tlr.Inorder();
-    cout << endl;
-
-    // RL Rotation
-    AVL trl;
-    trl.root = trl.rInsert(trl.root, 10);
-    trl.root = trl.rInsert(trl.root, 30);
-    trl.root = trl.rInsert(trl.root, 20);
-
-    trl.Inorder();
+    tree.Inorder();
     cout << endl;
 
     return 0;
