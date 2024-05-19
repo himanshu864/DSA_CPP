a090f4f79a16bbbf6ea01bbd86d7af83cc24f403
himanshu864 <himanshu864000@gmail.com>
2024-05-19 11:03:43 +0530

    morning graph hard later

---
+++ Guilt/Graph/MaxSumNodeVal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution
+{
+public:
+    long long maximumValueSum(vector<int> &nums, int k, vector<vector<int>> &edges)
+    {
+        long long sum = 0, cnt = 0, sacrifice = INT_MAX;
+        for (long long n : nums)
+        {
+            sum += max(n ^ k, n);
+            cnt += (n ^ k) > n;
+            sacrifice = min(sacrifice, abs(n - (n ^ k)));
+        }
+        return sum - (cnt % 2 ? sacrifice : 0);
+    }
+};
+
+int main()
+{
+    return 0;
+}
