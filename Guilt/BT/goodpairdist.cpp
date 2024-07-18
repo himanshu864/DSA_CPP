921544a4a878603d5c1cbbe21e1def0e635056b0
himanshu864 <himanshu96864@gmail.com>
2024-07-18 22:16:37 +0530

    daily guilt bt delhi bs

---
+++ Guilt/BT/goodpairdist.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int ans = 0;
+    int countPairs(TreeNode *root, int distance)
+    {
+        dfs(root, distance);
+        return ans;
+    }
+    // Idea is to perform dfs & return a list of distances of all leaf nodes to
+    // the ancestor and let the ancestor compare the distances of all it's leaf
+    // nodes, add them to result
+    vector<int> dfs(TreeNode *root, int d)
+    {
+        vector<int> p = {};
+        if (root == nullptr)
+            return p;
+
+        auto left = dfs(root->left, d);
+        auto right = dfs(root->right, d);
+
+        if (left.size() == 0 && right.size() == 0)
+        {
+            p.push_back(1);
+            return p;
+        }
+        // Compare distance and add them to answer
+        for (int i = 0; i < left.size(); i++)
+        {
+            for (int j = 0; j < right.size(); j++)
+            {
+                if (left[i] + right[j] <= d)
+                {
+                    ans++;
+                }
+            }
+        }
+        // Increase distance by one for all child and send them to parent
+        for (int i = 0; i < left.size(); i++)
+        {
+            left[i]++;
+            p.push_back(left[i]);
+        }
+
+        for (int i = 0; i < right.size(); i++)
+        {
+            right[i]++;
+            p.push_back(right[i]);
+        }
+
+        return p;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+
+    int distance = 3;
+    cout << sol.countPairs(root, distance) << endl;
+    return 0;
+}
+
+// https://leetcode.com/problems/number-of-good-leaf-nodes-pairs
