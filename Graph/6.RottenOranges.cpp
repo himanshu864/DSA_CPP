1b3c96947afd89a77c96099984a84232d56c1ee8
himanshu864 <himanshu96864@gmail.com>
2024-08-04 14:00:04 +0530

    good rotten orange bfs

--- Graph/6.RottenOranges.cpp
+++ Graph/6.RottenOranges.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-class solution
+class Solution
 {
 public:
-    int orangesrotting(vector<vector<int>> &grid)
+    int orangesRotting(vector<vector<int>> &grid)
     {
         int ans = 0;
         int m = grid.size(), n = grid[0].size();
-        while (true)
-        {
-            vector<pair<int, int>> rotten;
-            for (int r = 0; r < m; r++)
-                for (int c = 0; c < n; c++)
-                    if (grid[r][c] == 2)
-                        rotten.push_back({r, c});
+        int fresh = 0;
+        vector<pair<int, int>> fd = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        queue<pair<int, int>> rotten;
 
-            bool flag = true;
-            for (auto p : rotten)
+        // bfs with intial rotten
+        for (int r = 0; r < m; r++)
+            for (int c = 0; c < n; c++)
             {
-                int r = p.first, c = p.second;
-                if (grid[p.first][p.second] == 2)
-                {
-                    if (r > 0 && grid[r - 1][c] == 1)
-                        grid[r - 1][c] = 2, flag = false;
-                    if (c > 0 && grid[r][c - 1] == 1)
-                        grid[r][c - 1] = 2, flag = false;
-                    if (r + 1 < m && grid[r + 1][c] == 1)
-                        grid[r + 1][c] = 2, flag = false;
-                    if (c + 1 < n && grid[r][c + 1] == 1)
-                        grid[r][c + 1] = 2, flag = false;
-                }
+                if (grid[r][c] == 1)
+                    fresh++;
+                if (grid[r][c] == 2)
+                    rotten.push({r, c});
             }
-            if (flag)
-                break;
-            ans++;
-        }
 
-        for (auto i : grid)
-            for (int j : i)
-                if (j == 1)
-                    return -1;
-        return ans;
+        // V.IMP fresh check
+        while (rotten.size() && fresh)
+        {
+            ans++;
+            for (int s = rotten.size(); s; s--)
+            {
+                for (auto d : fd)
+                {
+                    int nr = rotten.front().first + d.first;
+                    int nc = rotten.front().second + d.second;
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1)
+                    {
+                        fresh--;
+                        grid[nr][nc] = 2;
+                        rotten.push({nr, nc});
+                    }
+                }
+                rotten.pop();
+            }
+        }
+        return fresh ? -1 : ans;
     }
 };
 
 int main()
 {
-    solution sol;
+    Solution sol;
     vector<vector<int>> grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
-    cout << sol.orangesrotting(grid) << endl;
+    cout << sol.orangesRotting(grid) << endl;
     return 0;
 }
+
+// https://leetcode.com/problems/rotting-oranges/
