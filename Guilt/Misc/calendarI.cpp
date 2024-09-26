977598c61b9647018f5e65122051a18640f33449
Himanshu Aggarwal <himanshu864000@gmail.com>
2024-09-26 13:51:22 +0530

    daily guilt fullmarks software engineering

---
+++ Guilt/Misc/calendarI.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MyCalendar
+{
+    vector<pair<int, int>> books;
+
+public:
+    bool book(int start, int end)
+    {
+        for (pair<int, int> p : books)
+            if (max(p.first, start) < min(end, p.second))
+                return false;
+        books.push_back({start, end});
+        return true;
+    }
+};
+
+int main()
+{
+    int n = 4;
+    cout << n << endl;
+    return 0;
+}
+
+// https://leetcode.com/problems/my-calendar-i/
