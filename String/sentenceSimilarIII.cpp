4120740389d93ac03c36c54115969ffcb29db438
himanshu864 <himanshu864000@gmail.com>
2024-10-07 14:09:22 +0530

    daily optimize dandiya

--- String/sentenceSimilarIII.cpp
+++ String/sentenceSimilarIII.cpp
@@ -18,52 +18,32 @@ class Solution
         return w;
     }
 
-    bool checkson(vector<string> w1, vector<string> w2)
-    {
-        // check if:
-        // - w2 contains all words from w1,
-        // - in same order,
-        // - with just a few extra words in one gap
-        // we shuold remove from both sides for special test cases, since only one gap
-        int i = 0, j = 0;
-        int ix = w1.size() - 1, jx = w2.size() - 1;
-        bool free = true;
-        while (i <= ix && j <= jx)
-        {
-            if (w1[i] == w2[j])
-            {
-                i++;
-                j++;
-            }
-            else if (w1[ix] == w2[jx])
-            {
-                ix--;
-                jx--;
-            }
-            else if (free)
-            {
-                free = false;
-                while (j <= jx && w2[j] != w1[i])
-                    j++;
-            }
-            else
-                return false;
-        }
-        if (i <= ix)
-            return false;
-        return j > jx || free;
-    }
-
 public:
     bool areSentencesSimilar(string s1, string s2)
     {
         if (s1.size() == s2.size())
             return s1 == s2;
 
-        // hash words with their order
+        // split into words
         vector<string> w1 = hashwords(s1), w2 = hashwords(s2);
 
-        return (s1.size() < s2.size() ? checkson(w1, w2) : checkson(w2, w1));
+        // ensure w1 is smaller
+        if (w1.size() > w2.size())
+            swap(w1, w2);
+
+        int n1 = w1.size();
+        int i = 0, j = 0;
+
+        // remove common prefix
+        while (i < n1 && w1[i] == w2[i])
+            i++;
+
+        // remove common suffix
+        while (j < n1 && w1[n1 - 1 - j] == w2[w2.size() - 1 - j])
+            j++;
+
+        // ensure that clears smaller sentence
+        return i + j >= n1;
     }
 };
 
@@ -72,8 +52,8 @@ int main()
     Solution sol;
     string s1 = "This baby";
     string s2 = "This is my baby";
-    string s1 = "A";
-    string s2 = "a A b A";
+    // string s1 = "A";
+    // string s2 = "a A b A";
     cout << sol.areSentencesSimilar(s1, s2) << endl;
     return 0;
 }
